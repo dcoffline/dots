8d422850d05427c740523228da449d11d1ec2459
dcoffline <eric@hemati.com>
2026-06-03 13:01:22 -0500

    Updated shell aliases

--- config/bash/alias.bash
+++ config/bash/alias.bash
@@ -4,7 +4,7 @@
 
 if [ -f /run/.containerenv ]; then
 
-  [ command -v /usr/bin/fastfetch ] && fastfetch
+  [ -f /usr/bin/fastfetch ] && fastfetch
 
   # Set Podman socket path for Container
   export DOCKER_HOST="unix:///run/host/run/user/$(id -u)/podman/podman.sock"
