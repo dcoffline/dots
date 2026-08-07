8058d8bd09055ad3564d58436018192c9b14dec7
dcoffline <eric@hemati.com>
2026-08-07 13:36:51 -0500

    Updated shell aliases

--- config/bash/alias.bash
+++ config/bash/alias.bash
@@ -40,7 +40,7 @@ else
   # Distrobox
   alias def='distrobox enter fedora'
   alias defr='distrobox enter fedora -e'
-  alias dea='distrobox enter athena -e cd $HOME'
+  alias dea='distrobox enter athena'
   alias dear='distrobox enter athena -e'
 
   # Rclone (Native)
